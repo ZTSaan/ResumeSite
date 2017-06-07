243a951cecc1ef037e12d10be3e54a537df1fc67
Muhammad Hassaan Siddiqui <ztsaan07@gmail.com>
2017-06-07 17:27:08 -0500

    Add files via upload

---
+++ ResumeSite/ResumeSite.Data/Objects/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResumeSite.Data.Objects
+{
+    public class Person
+    {
+        public string ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public Person(string _id, string _firstName, string _lastName, string _email)
+        {
+
+            ID = _id;
+            FirstName = _firstName;
+            LastName = _lastName;
+            Email = _email;
+        }
+    }
+}
---
+++ ResumeSite/ResumeSite/Pages/Registration.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="ResumeSite.Pages.Registration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+    
+<body>
+    <h2 style="text-align:center;">Registration</h2>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+
+                </tr>
+                <tr>
+
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
